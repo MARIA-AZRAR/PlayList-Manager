ee1e6d06df2c3f52385975b96bac311b5e04a3cb
MARIA-AZRAR <mariaazrar2000@gmail.com>
2019-10-04 23:29:47 +0500

    Show status to give current status of playlist

--- Playlist.cpp
+++ Playlist.cpp
@@ -17,3 +17,16 @@ playlist::playlist()
 	capacity = 2;                                       //capacity of array is 2
 	plist = new Song[m];                     //Creating a dynamic memory allocation 
 }
+
+				/*---------------------------------------------------------------------------------------------------------
+				-------------------------------------          SHOW STATUS        -----------------------------------------
+				----------------------------------------------------------------------------------------------------------*/
+
+void playlist::ShowStatus() const                    //Function to show the status of the playlist
+{
+	cout << "Size of the song array : " << capacity << endl; //this will show total size of array
+	cout << "Number of Songs in the playlist : " << scounter << endl;           //this willshow the total number of songs
+	cout << "Current song index : " << index << endl;        //this will show current index of the song
+}
+
+	
